abd7e089e288bd67f4e37bc8f3c8fcf0fe902253
ChainBotMaster <fr8.anime@gmail.com>
2018-02-03 00:19:20 +0800

    Add files via upload

--- bot/helpers.js
+++ bot/helpers.js
@@ -3,27 +3,15 @@ let role = config.get("role");
 
 // Checks if user is allowed to use a command only for Chainster user
 exports.hasPermsChainster = function(msg) {
-    if (msg.member.roles.some(r => role.chainster.includes(r.name))) {
-        return true;
-    } else {
-        return false;
-    }
+    return msg.member.roles.some(r => role.chainster.includes(r.name));
 };
 
 // Checks if user is allowed to use a command only for Support user
 exports.hasPermsSupport = function(msg) {
-    if (msg.member.roles.some(r => role.support.includes(r.name))) {
-        return true;
-    } else {
-        return false;
-    }
+    return msg.member.roles.some(r => role.support.includes(r.name));
 };
 
 // Check if command was sent in dm
 exports.inPrivate = function(msg) {
-    if (msg.channel.type == "dm") {
-        return true;
-    } else {
-        return false;
-    }
+    return msg.channel.type == "dm";
 };
